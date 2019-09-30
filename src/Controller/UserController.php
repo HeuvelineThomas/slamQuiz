cda116a034b750c1fcb5136ffaf58852b8d91f01
heuvtho <heuvtho@ndlp.fr>
2019-09-30 12:52:35 +0200

    ajout de bootstrap

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -22,6 +22,10 @@ class UserController extends AbstractController
         $userList[0]['last_name'] = "Dupont";
         $userList[1]['first_name'] = "Bertrant";
         $userList[1]['last_name'] = "Dumaine";
+        $userList[2]['first_name'] = "Bernard";
+        $userList[2]['last_name'] = "Durand";
+        $userList[3]['first_name'] = "Patrick";
+        $userList[3]['last_name'] = "Duchemin";
         
 
         return $this->render('user/user.html.twig', [
