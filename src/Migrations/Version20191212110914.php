20cfd463f73090041b62e3f349adbd389c7b417a
heuvtho <heuvtho@ndlp.fr>
2019-12-12 12:29:38 +0100

    [Core] Create entity answer

---
+++ src/Migrations/Version20191212110914.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191212110914 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE tbl_answer DROP CONSTRAINT fk_577b239aaa334807');
+        $this->addSql('DROP SEQUENCE tbl_question_id_seq CASCADE');
+        $this->addSql('DROP TABLE tbl_question');
+        $this->addSql('DROP INDEX idx_577b239aaa334807');
+        $this->addSql('ALTER TABLE tbl_answer DROP answer_id');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('CREATE SEQUENCE tbl_question_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE tbl_question (id INT NOT NULL, text VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('ALTER TABLE tbl_answer ADD answer_id INT NOT NULL');
+        $this->addSql('ALTER TABLE tbl_answer ADD CONSTRAINT fk_577b239aaa334807 FOREIGN KEY (answer_id) REFERENCES tbl_question (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX idx_577b239aaa334807 ON tbl_answer (answer_id)');
+    }
+}
